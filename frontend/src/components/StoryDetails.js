5f3fe3a51b0a1f40a0b2f561b3fbe9ca4d06177b
lspain0 <lucasspain0@gmail.com>
2024-01-21 13:47:35 +1300

    Layout changes

--- frontend/src/components/StoryDetails.js
+++ frontend/src/components/StoryDetails.js
@@ -1,13 +1,27 @@
 function truncate(str, n){
-    return (str.length > n) ? str.slice(0, n-1) + '&hellip;' : str;
+    return (str.length > n) ? str.slice(0, n-1) + '...' : str;
   };
+
+
+
+function removeTags(str) {
+    if ((str === null) || (str === ''))
+        return false;
+    else
+        str = str.toString();
+ 
+    // Regular expression to identify HTML tags in
+    // the input string. Replacing the identified
+    // HTML tag with a null string.
+    return str.replace(/(<([^>]+)>)/ig, '');
+}
   
 
 const StoryDetails = ({ story }) => {
     return (
         <div className="story-card">
-            <h4 className="story-h4">{truncate(story.content, 30)}</h4>
-            <p className="story-p">{truncate(story.content, 60)}</p>
+            <h4 className="story-h4">{truncate(removeTags(story.title), 30)}</h4>
+            <p className="story-p">{truncate(removeTags(story.content), 60)}</p>
             <sub className="story-children">{story.children}</sub>
             <sub className="story-sub">{"Story shared by Author\n"+story.createdAt}</sub>
         </div>
