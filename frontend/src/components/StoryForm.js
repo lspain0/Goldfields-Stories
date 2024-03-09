8c84060fa9319b38aeadeaed63362dd28069acbf
lspain0 <lucasspain0@gmail.com>
2024-03-09 14:55:51 +1300

    Load user and content for edited stories from mongodb

--- frontend/src/components/StoryForm.js
+++ frontend/src/components/StoryForm.js
@@ -11,8 +11,7 @@ import { useLocation, Link } from 'react-router-dom';
 var editing = false;
 
 function isEditing() {
-  if (window.location.pathname.includes('editstory'))
-  {
+  if (window.location.pathname.includes('editstory')) {
     editing = true;
   }
 };
@@ -56,7 +55,6 @@ const UploadWidget = ({ onImageUpload }) => {
 
 const StoryForm = () => {
   isEditing();
-  const [currentStory, setCurrentStory] = useState(null);
   const { dispatch } = useStoriesContext();
   const [title, setTitle] = useState('');
   const [children, setChildren] = useState([]);
@@ -68,21 +66,23 @@ const StoryForm = () => {
   const [selectedCheckTreeValuesTags, setSelectedCheckTreeValuesTags] = useState([]);
   const [selectedCheckTreeValuesChildren, setSelectedCheckTreeValuesChildren] = useState([]);
   const quillRef = useRef();
-  const currentUser = localStorage.getItem("name");
+  const [currentUser, setUser] = useState(localStorage.getItem("name"));
 
   useEffect(() => {
     const fetchStoryById = async () => {
       if (editing === true) {
         const storyId = window.location.pathname.split('/')[2];
-  
+
         if (storyId) {
           try {
             const response = await fetch(`/api/stories/${storyId}`);
             const json = await response.json();
-  
+
             if (response.ok) {
-              setCurrentStory(json);
-              setTitle(json.title); // Set the title to the title of the current story
+              setTitle(json.title);
+              setChildren(json.children || []); // Set children from fetched data or use empty array if null
+              setContent(json.content);
+              setUser(json.author);
             } else {
               console.error(`Error fetching story with ID ${storyId}:`, json);
             }
@@ -90,71 +90,17 @@ const StoryForm = () => {
             console.error(`Error fetching story with ID ${storyId}:`, error);
           }
         }
+      } else {
+        // If not in edit mode, set default placeholders or values
+        setTitle('Default Title');
+        setChildren([]); // Set empty array as children placeholder
+        setContent(''); // Set empty string as content placeholder
       }
     };
-  
+
     fetchStoryById();
   }, []);
-  
-  const handleImageUpload = imageUrl => {
-    setContent(content + `\n<img src="${imageUrl}" alt="uploaded" />\n`);
-  };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-  
-    // Ensure that children is always an array of objects
-    const childrenArray = Array.isArray(children) ? children : [{ value: children }];
-  
-    const childrenString = childrenArray.map((child) => child.value).join(',');
-    const tagsString = tags;
-    const story = {
-      title,
-      children: childrenString,
-      tags: tagsString,
-      content,
-      categories: selectedCategories.join(','),
-      author: currentUser,
-      state,
-    };
-  
-    try {
-      const response = await fetch('/api/stories', {
-        method: 'POST',
-        body: JSON.stringify(story),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-  
-      const json = await response.json();
-  
-      if (!response.ok) {
-        alert(json.error);
-      }
-  
-      if (response.ok) {
-        setTitle('');
-        setChildren([]);
-        setTags('');
-        setContent('');
-        setState('');
-        setSelectedCategories([]);
-        setError(null);
-        console.log('New Story Posted', json);
-        dispatch({ type: 'CREATE_STORY', payload: json });
-
-        alert("Story Posted!");
-        setTimeout(function() {
-          window.location.href = '/stories';
-      }, 1);
-      }
-    } catch (error) {
-      console.error('Error submitting the form:', error);
-      setError('An error occurred while submitting the form.');
-    }
-  };
-  
   useEffect(() => {
     if (quillRef.current) {
       // Additional setup or handling can be added here
@@ -177,84 +123,139 @@ const StoryForm = () => {
     setChildren(childrenString);
   };
 
-  if (!currentStory && isEditing === true) {
-    return null;
-  }
-  else {
-    return (
-      <body className="story-form">
-        <form className="create-story" onSubmit={handleSubmit}>
-          <div className="input-container">
-            <input
-              className="short-input"
-              placeholder={"Story title..."}
-              type="text"
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <span className="author-input" readOnly>
-              Author: {getName(currentUser)}
-            </span>
-          </div>
-  
-          <CheckTreePicker className="check-tree"
-            placeholder="Add children to this story..."
-            data={StudentList()}
-            uncheckableItemValues={['1-1', '1-1-2']}
-            value={selectedCheckTreeValuesChildren}
-            onChange={handleCheckTreePickerChangeChildren}
-            cascade={false}
-            style={{ width: 660 }}
-          />
-          
-          <CheckTreePicker
-            className="check-tree2"
-            placeholder="Learning tags..."
-            data={groupedTags}
-            uncheckableItemValues={['1', '2', '3', '4']}
-            value={selectedCheckTreeValuesTags}
-            onChange={handleCheckTreePickerChangeTags}
-            cascade={false}
-            style={{ width: 660 }}
-            renderMenu={(menu) => (
-              <div style={{ maxWidth: 'calc(100vh - 100px)'}}>
-                {menu}
-              </div>
-            )}
-          />
-  
-          <UploadWidget onImageUpload={handleImageUpload} />
-  
-          <div className="quill">
-            <ReactQuill
-              ref={quillRef}
-              placeholder="Start writing..."
-              onChange={(value) => setContent(value)}
-              value={content}
-              modules={{
-                toolbar: {
-                  container: [
-                    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-                    ['bold', 'italic', 'underline', 'strike'],
-                    ['blockquote'],
-                    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-                    [{ 'align': [] }],
-                    [{ 'color': [] }, { 'background': [] }],
-                    ['clean']
-                  ],
-                },
-              }}
-              style={{ height: 'calc(100vh - 280px)' }}
-            />
-          </div>
-  
-          <div className="centered-button">
-            <button className="story-form-button">Post Story</button>
-          </div>
-          {error && <div className="error">{error}</div>}
-        </form>
-      </body>
-    );
+  const handleImageUpload = imageUrl => {
+    setContent(content + `\n<img src="${imageUrl}" alt="uploaded" />\n`);
   };
-  }
-  export default StoryForm;
-  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Ensure that children is always an array of objects
+    const childrenArray = Array.isArray(children) ? children : [{ value: children }];
+
+    const childrenString = childrenArray.map((child) => child.value).join(',');
+    const tagsString = tags;
+    const story = {
+      title,
+      children: childrenString,
+      tags: tagsString,
+      content,
+      categories: selectedCategories.join(','),
+      author: currentUser,
+      state,
+    };
+
+    try {
+      const response = await fetch('/api/stories', {
+        method: 'POST',
+        body: JSON.stringify(story),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const json = await response.json();
+
+      if (!response.ok) {
+        alert(json.error);
+      }
+
+      if (response.ok) {
+        setTitle('');
+        setChildren([]);
+        setTags('');
+        setContent('');
+        setState('');
+        setSelectedCategories([]);
+        setError(null);
+        console.log('New Story Posted', json);
+        dispatch({ type: 'CREATE_STORY', payload: json });
+
+        alert("Story Posted!");
+        setTimeout(function() {
+          window.location.href = '/stories';
+        }, 1);
+      }
+    } catch (error) {
+      console.error('Error submitting the form:', error);
+      setError('An error occurred while submitting the form.');
+    }
+  };
+
+  return (
+    <body className="story-form">
+      <form className="create-story" onSubmit={handleSubmit}>
+        <div className="input-container">
+          <input
+            className="short-input"
+            placeholder={"Story title..."}
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <span className="author-input" readOnly>
+            Author: {getName(currentUser)}
+          </span>
+        </div>
+
+        <CheckTreePicker className="check-tree"
+          placeholder="Add children to this story..."
+          data={StudentList()}
+          uncheckableItemValues={['1-1', '1-1-2']}
+          value={selectedCheckTreeValuesChildren}
+          onChange={handleCheckTreePickerChangeChildren}
+          cascade={false}
+          style={{ width: 660 }}
+        />
+        
+        <CheckTreePicker
+          className="check-tree2"
+          placeholder="Learning tags..."
+          data={groupedTags}
+          uncheckableItemValues={['1', '2', '3', '4']}
+          value={selectedCheckTreeValuesTags}
+          onChange={handleCheckTreePickerChangeTags}
+          cascade={false}
+          style={{ width: 660 }}
+          renderMenu={(menu) => (
+            <div style={{ maxWidth: 'calc(100vh - 100px)'}}>
+              {menu}
+            </div>
+          )}
+        />
+
+        <UploadWidget onImageUpload={handleImageUpload} />
+
+        <div className="quill">
+          <ReactQuill
+            ref={quillRef}
+            placeholder="Start writing..."
+            onChange={(value) => setContent(value)}
+            value={content}
+            modules={{
+              toolbar: {
+                container: [
+                  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                  ['bold', 'italic', 'underline', 'strike'],
+                  ['blockquote'],
+                  [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+                  [{ 'align': [] }],
+                  [{ 'color': [] }, { 'background': [] }],
+                  ['clean']
+                ],
+              },
+            }}
+            style={{ height: 'calc(100vh - 280px)' }}
+          />
+        </div>
+
+        <div className="centered-button">
+          <button className="story-form-button">Post Story</button>
+        </div>
+        {error && <div className="error">{error}</div>}
+      </form>
+    </body>
+  );
+};
+
+export default StoryForm;
