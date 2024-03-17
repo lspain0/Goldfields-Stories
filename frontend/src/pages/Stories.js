021e3bb3e262adef8130657a3c813bac6d6a2488
lspain0 <lucasspain0@gmail.com>
2024-03-17 16:47:00 +1300

    Fixed dropdown toggle area

--- frontend/src/pages/Stories.js
+++ frontend/src/pages/Stories.js
@@ -85,7 +85,7 @@ const Stories = () => {
   //closes dropdown when user clicks outside of it
   useEffect(() => {
     const closeDropdown = (event) => {
-      if (!event.target.closest('.filter-story-dropdown')) {
+      if (!event.target.closest('.rs-dropdown-toggle')) {
         setDropdownOpen(false);
       }
     };
