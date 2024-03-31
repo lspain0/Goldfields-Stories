7e91793e721c9df8a69ccf3299f9572d3dfa481d
fkaidaw <90433381+fkaidaw@users.noreply.github.com>
2024-04-01 10:11:54 +1300

    Updated SearchStories

--- frontend/src/pages/SearchStories.js
+++ frontend/src/pages/SearchStories.js
@@ -13,20 +13,21 @@ const SearchStories = () => {
 
   const initiateSearch = async () => {
     const term = searchTerm.trim();
-
+    setError(''); // Clear any existing errors before starting a new search
+    setStories([]); // Clear previous stories right at the start of a new search
+  
     if (!term) {
-      setStories([]); // Clear stories if search term is empty
       return;
     }
-
+  
     try {
-        const response = await axios_obj.get(`/stories/search?search=${term}`);
+      const response = await axios_obj.get(`/stories/search?search=${term}`);
       if (response.status === 200) {
         setStories(response.data);
       }
     } catch (error) {
       console.error('Error searching for stories:', error);
-      setError('Failed to fetch stories. Please try again.'); // Setting error message
+      setError('No stories found. Please try again.'); // Setting error message
     }
   };
 
@@ -41,16 +42,14 @@ const SearchStories = () => {
       <button onClick={initiateSearch}>Search</button> {/* Search button */}
       {error && <p className="error">{error}</p>} {/* Display error message if any */}
       <div className="story-cards-container">
-        {stories.length > 0 ? (
+        {stories.length > 0 && (
           stories.map(story => (
             <StoryDetails story={story} key={story._id} />
           ))
-        ) : (
-          <p>No stories found.</p> // Message displayed when no stories are found
         )}
       </div>
     </div>
-  );
+  );  
 };
 
 export default SearchStories;
