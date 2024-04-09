3634180f658cdc2a5424ca2380c8bd84d15147cb
itsbiju <bhijendrabhattarai@gmail.com>
2024-04-09 20:15:56 +1200

    Removal of logo and centered text

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -71,11 +71,11 @@ function SignUP() {
       `}
       </style>
       <div className="class-form-container-login">
-        <div className="logo-container2">
+        {/* <div className="logo-container2">
           <Logo />
-        </div>
+        </div> */}
         <div className="">
-          <h1 className="heading-login">CREATE ACCOUNT</h1>
+        <h1 style={{textAlign:"center",fontSize:"40px"}} className="heading-login">CREATE ACCOUNT</h1>
           <div className="">
 
             <form onSubmit={handleSubmit} className="class-form">
